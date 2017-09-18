0553b04be103b969fc3ddbab2128028df92443e4
nosthertus <oscarr.reyes17@gmail.com>
2017-09-18 03:49:45 -0500

    + Minified build
    *Added minified function to create a minify version of the build

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp   = require("gulp");
 var concat = require("gulp-concat-util");
+var minify = require("gulp-minify");
+var rename = require("gulp-rename");
 
 gulp.task("js", () => {
 	return gulp.src("src/javascript/**/*.js")
 		.pipe(concat("native-material.js", {sep: "\n\n"}))
+		.pipe(gulp.dest("./build/"))
+		.pipe(minify({
+			ext: {
+				min: ".min.js"
+			}
+		}))
 		.pipe(gulp.dest("./build/"));
 });
