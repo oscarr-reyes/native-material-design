31755a29890717e8994c8d05b4ebbdf26a9a4b48
nosthertus <oscarr.reyes17@gmail.com>
2017-10-09 03:34:36 -0500

    + Sass source files
    *Added SASS source files for the library

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp   = require("gulp");
 var concat = require("gulp-concat-util");
 var minify = require("gulp-minify");
 var rename = require("gulp-rename");
+var sass   = require("gulp-sass");
 
 gulp.task("js", () => {
 	return gulp.src("src/javascript/**/*.js")
@@ -13,4 +14,18 @@ gulp.task("js", () => {
 			}
 		}))
 		.pipe(gulp.dest("./build/"));
+});
+
+gulp.task("css", () => {
+	let sassOptions = {
+		outputStyle: "expanded"
+	};
+
+	return gulp.src("src/stylesheet/native-material.scss")
+		.pipe(sass(sassOptions).on("error", sass.logError))
+		.pipe(gulp.dest("./build/stylesheet/"));
+});
+
+gulp.task("css:watch", () => {
+	return gulp.watch("src/stylesheet/**/*.scss", ["css"]);
 });
